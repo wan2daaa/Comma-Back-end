5e7006ff4f872f912ff15e4a32ce26e5c94e5202
given02 <39878215+given02@users.noreply.github.com>
2023-02-24 11:37:44 +0900

    swagger 세팅 수정

--- src/main/java/com/team/comma/controller/TestController.java
+++ src/main/java/com/team/comma/controller/TestController.java
@@ -24,15 +24,4 @@ public class TestController {
         return result;
     }
 
-    @Operation(summary = "test hello", description = "hello api example")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK !!"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
-            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
-    })
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "Park") @RequestParam String name) {
-        return ResponseEntity.ok("hello " + name);
-    }
 }
