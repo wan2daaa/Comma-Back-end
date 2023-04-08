700a0d64b03b36f98564e35b38adab40a1bbff07
wan2daaa <wan2daaa@gmail.com>
2023-04-08 16:05:37 +0900

    CMA-62 [feature] : MessageResponse Data 타입을 제니릭으로 변경 및 변수명 리팩토링 ( userEntity -> user ...)

--- src/main/java/com/team/comma/service/UserService.java
+++ src/main/java/com/team/comma/service/UserService.java
@@ -33,64 +33,64 @@ public class UserService {
 	final private JwtTokenProvider jwtTokenProvider;
 
 	public MessageResponse login(final LoginRequest loginRequest) throws AccountException {
-		User userEntity = userRepository.findByEmail(loginRequest.getEmail());
+		User user = userRepository.findByEmail(loginRequest.getEmail());
 
-		if (userEntity == null) {
+		if (user == null) {
 			throw new AccountException("정보가 올바르지 않습니다.");
 		}
-		
-		if (userEntity.getUserType() == UserType.OAuthUser) {
+
+		if (user.getUserType() == UserType.OAuthUser) {
 			throw new AccountException("일반 사용자는 OAuth 계정으로 로그인할 수 없습니다.");
 		}
 		
-		if(!userEntity.getPassword().equals(loginRequest.getPassword())) {
+		if(!user.getPassword().equals(loginRequest.getPassword())) {
 			throw new AccountException("정보가 올바르지 않습니다.");
 		}
 
-		createJwtCookie(userEntity);
+		createJwtCookie(user);
 
-		return MessageResponse.builder()
+		return MessageResponse.<User>builder()
 				.code(ResponseCode.LOGIN_SUCCESS)
 				.message("로그인이 성공적으로 되었습니다.")
-				.data(userEntity.getEmail())
+				.data(user)
 			.build();
 	}
 
 	public MessageResponse register(final RegisterRequest registerRequest) throws AccountException {
-		User userEntity = userRepository.findByEmail(registerRequest.getEmail());
+		User findUser = userRepository.findByEmail(registerRequest.getEmail());
 
-		if (userEntity != null) {
+		if (findUser != null) {
 			throw new AccountException("이미 존재하는 계정입니다.");
 		}
 
 		User buildEntity = createUser(registerRequest, UserType.GeneralUser);
 
-		User result = userRepository.save(buildEntity);
+		User savedUser = userRepository.save(buildEntity);
 
-		return MessageResponse.builder()
+		return MessageResponse.<User>builder()
 			.code(ResponseCode.REGISTER_SUCCESS)
 			.message("성공적으로 가입되었습니다.")
-			.data(result.getEmail())
+			.data(savedUser)
 			.build();
 	}
 
 	public MessageResponse loginOauth(final RegisterRequest registerRequest) throws AccountException {
-		User userEntity = userRepository.findByEmail(registerRequest.getEmail());
+		User findUser = userRepository.findByEmail(registerRequest.getEmail());
 
-		if (userEntity == null) { // 정보가 없다면 회원가입
-			User Entity = createUser(registerRequest, UserType.OAuthUser);
+		if (findUser == null) { // 정보가 없다면 회원가입
+			User createUser = createUser(registerRequest, UserType.OAuthUser);
 
-			userEntity = userRepository.save(Entity);
-		} else if (userEntity.getUserType() == UserType.GeneralUser) { // 일반 사용자가 존재한다면
+			findUser = userRepository.save(createUser);
+		} else if (findUser.getUserType() == UserType.GeneralUser) { // 일반 사용자가 존재한다면
 			throw new AccountException("일반 사용자가 이미 존재합니다.");
 		}
 
-		createJwtCookie(userEntity);
+		createJwtCookie(findUser);
 
-		return MessageResponse.builder()
+		return MessageResponse.<User>builder()
 			.code(ResponseCode.LOGIN_SUCCESS)
 			.message("로그인이 성공적으로 되었습니다.")
-			.data(userEntity.getEmail())
+			.data(findUser)
 			.build();
 	}
 
