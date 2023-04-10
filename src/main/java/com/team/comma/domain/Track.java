782296217c874222eaddedeaaafe430dfec21b74
given02 <votmdnjem_id@naver.com>
2023-04-11 01:17:14 +0900

    refactor: 프로젝트 리팩토링 archive -> archive_tb schema-local 오타 수정 track, playlist 연관관계 수정 UserServiceTest 에러 코드 주석처리

--- src/main/java/com/team/comma/domain/Track.java
+++ src/main/java/com/team/comma/domain/Track.java
@@ -1,15 +1,7 @@
 package com.team.comma.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
@@ -35,5 +27,9 @@ public class Track extends BaseEntity {
 
     private Boolean albumFlag;
 
+    @Setter
+    @JoinColumn(name = "playlist_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Playlist playlist;
 
 }
