08082e883b56953dacf84bf1e056503157a3e73a
given02 <votmdnjem_id@naver.com>
2023-04-14 01:32:55 +0900

    feat: 코드 리뷰 수정사항 반영 playlistService 필요없는 어노테이션 정리 및 extractMethod 분리하여 코드 정리

--- src/main/java/com/team/comma/service/PlaylistService.java
+++ src/main/java/com/team/comma/service/PlaylistService.java
@@ -6,7 +6,6 @@ import com.team.comma.dto.PlaylistResponse;
 import com.team.comma.dto.PlaylistTrackResponse;
 import com.team.comma.repository.PlaylistRepository;
 import com.team.comma.repository.PlaylistTrackRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class PlaylistService {
 
@@ -30,22 +28,30 @@ public class PlaylistService {
     }
 
     public List<PlaylistResponse> getPlaylistResponse(final String email) {
-        List<PlaylistResponse> result = new ArrayList<>();
-        List<PlaylistTrackResponse> tracks = new ArrayList<>();
-
         List<Playlist> userPlaylist = getPlaylist(email); // userEmail로 playlist 조회
+        List<PlaylistResponse> result = createPlaylist(userPlaylist);
+        return result;
+    }
+
+    public List<PlaylistResponse> createPlaylist(List<Playlist> userPlaylist){
+        List<PlaylistResponse> result = new ArrayList<>();
         if(userPlaylist.size() != 0){
             for(Playlist playlist : userPlaylist){
                 List<PlaylistTrack> playlistTracks = getPlaylistTrack(playlist.getId()); // playlistId로 track 조회
-                if(playlistTracks.size() != 0) {
-                    for (PlaylistTrack playlistTrack : playlistTracks) {
-                        tracks.add(PlaylistTrackResponse.of(playlistTrack.getTrack()));
-                    }
-                }
+                List<PlaylistTrackResponse> tracks = createPlaylistTracks(playlistTracks);
                 result.add(PlaylistResponse.of(playlist,tracks));
             }
         }
         return result;
     }
 
+    public List<PlaylistTrackResponse> createPlaylistTracks(List<PlaylistTrack> playlistTracks){
+        List<PlaylistTrackResponse> result = new ArrayList<>();
+        if(playlistTracks.size() != 0) {
+            for (PlaylistTrack playlistTrack : playlistTracks) {
+                result.add(PlaylistTrackResponse.of(playlistTrack.getTrack()));
+            }
+        }
+        return result;
+    }
 }
