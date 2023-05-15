685ec069a3558e3ac4abe10de6d59cd1c27064da
wan2daaa <wan2daaa@gmail.com>
2023-05-16 01:37:47 +0900

    refact : TrackServiceTest 에러 수정

--- src/test/java/com/team/comma/spotify/track/service/TrackServiceTest.java
+++ src/test/java/com/team/comma/spotify/track/service/TrackServiceTest.java
@@ -1,13 +1,18 @@
 package com.team.comma.spotify.track.service;
 
 import static com.team.comma.common.constant.ResponseCodeEnum.REQUEST_SUCCESS;
-import static com.team.comma.common.constant.ResponseCodeEnum.SIMPLE_REQUEST_FAILURE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import com.team.comma.common.dto.MessageResponse;
 import com.team.comma.spotify.playlist.repository.PlaylistTrackRepository;
+import com.team.comma.spotify.track.domain.Track;
+import com.team.comma.spotify.track.repository.TrackRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,10 +28,14 @@ class TrackServiceTest {
     @Mock
     PlaylistTrackRepository playlistTrackRepository;
 
+    @Mock
+    TrackRepository trackRepository;
 
     @Test
     void 트랙의_알람설정을_바꾼다(){
         //given
+        doReturn(Optional.of(Track.class))
+            .when(trackRepository).findById(anyLong());
         doReturn(1L)
             .when(playlistTrackRepository).changeAlarmFlagWithTrackId(anyLong());
 
@@ -43,16 +52,12 @@ class TrackServiceTest {
     @Test
     void 트랙의_알림설정을_바꾼다_실패(){
         //given
-        doReturn(0L)
-            .when(playlistTrackRepository).changeAlarmFlagWithTrackId(anyLong());
+        doThrow(EntityNotFoundException.class)
+            .when(trackRepository).findById(anyLong());
 
-        //when
-        MessageResponse messageResponse = trackService.updateAlarmFlag(anyLong());
-
-        //then
-        assertThat(messageResponse).isNotNull();
-        assertThat(messageResponse.getCode()).isEqualTo(SIMPLE_REQUEST_FAILURE.getCode());
-        assertThat(messageResponse.getMessage()).isEqualTo(SIMPLE_REQUEST_FAILURE.getMessage());
+        //when //then
+        assertThatThrownBy(() -> trackService.updateAlarmFlag(anyLong()))
+            .isInstanceOf(EntityNotFoundException.class);
 
     }
 }
