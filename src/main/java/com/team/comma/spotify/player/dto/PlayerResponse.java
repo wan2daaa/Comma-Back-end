845308a4b6e3542ef8f5a0cee6af2cb7ad07ee45
wan2daaa <wan2daaa@gmail.com>
2023-05-22 22:50:23 +0900

    refact: PlayerController 삭제후 PlayerResponse 이름 변경

--- src/main/java/com/team/comma/spotify/player/dto/PlayerResponse.java
+++ src/main/java/com/team/comma/spotify/player/dto/PlayerResponse.java
@@ -7,13 +7,13 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public final class PlayerResponseDto {
+public final class PlayerResponse {
 
     private final String spotifyAccessToken;
 
     private final String trackUri;
 
-    public static PlayerResponseDto of(String spotifyAccessToken, String trackUri) {
-        return new PlayerResponseDto(spotifyAccessToken, trackUri);
+    public static PlayerResponse of(String spotifyAccessToken, String trackUri) {
+        return new PlayerResponse(spotifyAccessToken, trackUri);
     }
 }
